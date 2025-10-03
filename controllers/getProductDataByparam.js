bfd48db067e3af2069cb31ed4cc8e81b29cc41aa
patilnagesh03 <nageshpatil7311@gmail.com>
2025-10-03 15:12:01 +0530

    Added filter functionality

--- controllers/getProductDataByparam.js
+++ controllers/getProductDataByparam.js
@@ -1,15 +1,28 @@
 import { productData } from "../productsData.js";
 
 export const getproductsDataByparam = (req, res) => {
-  const allowedFields = ["category", "name"];
+  const allowedFields = ["category", "name", "price"];
   const { fields, term } = req.params;
   if (!allowedFields.includes(fields)) {
     return res
       .status(400)
       .json("Endpoint not allowed. Please use only 'category', 'name'");
   }
-  const filteredProducts = productData.filter(
-    (product) => product[fields].toLowerCase() === term.toLowerCase()
-  );
+  let filteredProducts = [];
+
+  if (fields === "price") {
+    const mxPrice = Number(term);
+    if (isNaN(mxPrice)) {
+      return res.status(400).json("Price must be a valid number");
+    }
+    filteredProducts = productData.filter(
+      (product) => product[fields] <= mxPrice
+    );
+  } else {
+    filteredProducts = productData.filter(
+      (product) => product[fields].toLowerCase() === term.toLowerCase()
+    );
+  }
+
   res.json(filteredProducts);
 };
